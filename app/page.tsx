8941d7a6ab322c50749f6ea379ff0162ec6e9017
Mukul Pal <mukulpal0333@gmail.com>
2025-07-13 19:27:13 +0530

    feat: implement user synchronization and database integration with Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ModeToggle from "@/components/ModeToggle";
+import prisma from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       hello
